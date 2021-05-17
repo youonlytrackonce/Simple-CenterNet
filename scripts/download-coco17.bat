c9038442a819a0607468b2782f19ba60d7ef7605
Yonghye Kwon <developer.0hye@gmail.com>
2021-05-17 23:46:26 +0900

    cleanup

--- scripts/download-coco17.bat
+++ scripts/download-coco17.bat
@@ -2,6 +2,7 @@ mkdir dataset
 cd dataset
 mkdir coco17
 cd coco17
+
 wget http://images.cocodataset.org/zips/train2017.zip
 wget http://images.cocodataset.org/zips/val2017.zip
 wget http://images.cocodataset.org/zips/test2017.zip
