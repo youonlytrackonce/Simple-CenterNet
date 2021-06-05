161c0311ccb3a570b6a7d8c4c0bb9e112613cbad
Yonghye Kwon <developer.0hye@gmail.com>
2021-06-05 13:20:24 +0900

    faster augment_hsv

--- utils/transforms.py
+++ utils/transforms.py
@@ -357,12 +357,12 @@ def augment_hsv(img, hgain=0.0138, sgain=0.664, vgain=0.464): # https://github.c
     hue, sat, val = cv2.split(cv2.cvtColor(img, cv2.COLOR_BGR2HSV))
     dtype = img.dtype  # uint8
 
-    x = np.arange(0, 256, dtype=np.int16)
+    x = np.arange(0, 256, dtype=r.dtype)
     lut_hue = ((x * r[0]) % 180).astype(dtype)
     lut_sat = np.clip(x * r[1], 0, 255).astype(dtype)
     lut_val = np.clip(x * r[2], 0, 255).astype(dtype)
 
-    img_hsv = cv2.merge((cv2.LUT(hue, lut_hue), cv2.LUT(sat, lut_sat), cv2.LUT(val, lut_val))).astype(dtype)
+    img_hsv = cv2.merge((cv2.LUT(hue, lut_hue), cv2.LUT(sat, lut_sat), cv2.LUT(val, lut_val)))
     return cv2.cvtColor(img_hsv, cv2.COLOR_HSV2BGR)
 
 def cutout(img, max_n_holes=5):
