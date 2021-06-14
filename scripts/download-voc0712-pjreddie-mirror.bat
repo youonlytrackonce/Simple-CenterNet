8c9cb7a426b3eec71aae1be7b449cf06dcbd2fbe
Yonghye Kwon <developer.0hye@gmail.com>
2021-06-14 17:00:30 +0900

    Create download-voc0712-pjreddie-mirror.bat

---
+++ scripts/download-voc0712-pjreddie-mirror.bat
@@ -0,0 +1,8 @@
+mkdir dataset
+cd dataset
+wget http://pjreddie.com/media/files/VOCtrainval_11-May-2012.tar
+wget http://pjreddie.com/media/files/VOCtrainval_06-Nov-2007.tar
+wget http://pjreddie.com/media/files/VOCtest_06-Nov-2007.tar
+tar xvf VOCtrainval_06-Nov-2007.tar
+tar xvf VOCtest_06-Nov-2007.tar
+tar xvf VOCtrainval_11-May-2012.tar
